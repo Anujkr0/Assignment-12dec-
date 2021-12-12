533099bb0ba98fae90432d573216dd36a80fa939
Anujkr0 <92168995+Anujkr0@users.noreply.github.com>
2021-12-12 14:57:40 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,26 @@
+let a = fetch('https://meme-api.herokuapp.com/gimme');
+
+
+let b = a.then(function(response) {
+    return response.json();
+});
+
+b.then(function(data) {
+    
+    fn(data);
+});
+
+
+
+function fn(data) {
+    let h2 = document.createElement('h2');
+
+    let h3 = document.createElement('h3');
+    
+
+
+    let div = document.getElementById('response_div');
+    div.appendChild(h2);
+    div.appendChild(h3);
+    console.log(data);
+}
